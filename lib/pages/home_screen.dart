26f72bb3dca0a6acfe434874ee6227649c0173f9
“Nguirane120” <badaranguirane120@gmail.com>
2023-07-16 15:13:13 +0000

    custom appbar

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter_shop/utils.dart';
+import 'package:badges/badges.dart' as badges;
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Appcolors.primarycolor,
+                            blurRadius: 5,
+                            spreadRadius: 1)
+                      ]),
+                  child: Icon(
+                    Icons.sort,
+                    size: 30,
+                  ),
+                ),
+                Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Appcolors.primarycolor,
+                              blurRadius: 5,
+                              spreadRadius: 1)
+                        ]),
+                    child: badges.Badge(
+                      badgeContent: Text('3'),
+                      // badgeStyle: badges.BadgeStyle(padding: EdgeInsets.all(0)),
+                      child: Icon(Icons.notifications, size: 27,),
+                    )),
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
---
+++ lib/utils.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+
+class Appcolors{
+  static const primarycolor = Color(0xFF475269);
+  static const secondaryColr = Color(0xFFF5F9FD);
+}
